86eb51680a4813622902ee6bc699c3e934078944
Malcolm <malco.macarthur@gmail.com>
2019-01-19 23:29:45 -0700

    created python script

---
+++ requests_version.py
@@ -0,0 +1,8 @@
+import requests
+
+print("requests version: "+requests.__version__)
+
+# https://stackoverflow.com/questions/645312/what-is-the-quickest-way-to-http-get-in-python
+import urllib.request
+contents = urllib.request.urlopen("http://www.google.com/").read() 
+print(contents)
